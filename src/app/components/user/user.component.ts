8a04f044ab6952933de7e577849971d42b73f5ca
Janis Be <ja@janis>
2017-10-14 20:45:56 +0200

    2nd commit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+name:string;
+age:number;
+email:string;
+address:Address;
+hobbies:string[];
+hello:any;
 
   constructor() { }
 
   ngOnInit() {
+    this.name="asdasd";
+    this.age = 30;
+    this.address = {
+      street:'sztbka',
+      city:'wro',
+      state:'dolny slask'
+    }
+    this.hobbies = ['rower','lasert','koks'];
+    this.hello = 'hello';
   }
 
+}
+
+
+interface Address{
+  street:string,
+  city:string,
+  state:string
+  
 }
